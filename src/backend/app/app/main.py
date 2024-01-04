a596f09e684cd064abcbf989d8f179e49ab3f5e9
fjromeero <franjaviro2001@gmail.com>
2024-01-04 17:41:39 +0100

    chore: ⬆️ Add tenacity
    Signed-off-by: fjromeero <franjaviro2001@gmail.com>

--- src/backend/app/app/main.py
+++ src/backend/app/app/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
+from app.api.api import api_router
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.include_router(api_router)
