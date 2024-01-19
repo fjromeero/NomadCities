1ca9232caef7b6161dd7cdbbcc22e39324c47e71
fjromeero <franjaviro2001@gmail.com>
2024-01-19 12:48:02 +0100

    Add email to user db model
    Signed-off-by: fjromeero <franjaviro2001@gmail.com>

--- src/backend/app/app/db/models/user.py
+++ src/backend/app/app/db/models/user.py
@@ -7,6 +7,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique= True, index= True, nullable= False)
+    email = Column(String, unique=True, index=True)
     hashed_password=Column(String, nullable=False)
     is_admin = Column(Boolean, default= False)
     img = Column(String)
