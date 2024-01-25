14ddc55719339f54e3cbad6e97e1f0b0197ad874
fjromeero <franjaviro2001@gmail.com>
2024-01-25 23:25:33 +0100

    feat(frontend): Added logging utilities

---
+++ src/frontend/src/utils/users.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+const backendBaseUrl = `http://localhost:${process.env.BACKEND_PORT}`;
+
+export async function createUser(data) {
+    try {
+        const result = await axios.post(`${backendBaseUrl}/signup/`, data);
+        return result.data;
+    } catch (error) {
+        return error.response.data.detail;
+    }
+}
