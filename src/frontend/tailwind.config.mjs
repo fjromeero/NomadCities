05e7fef9619f97b83d83df9cf1ee19cd3ce92a33
fjromeero <franjaviro2001@gmail.com>
2024-01-26 16:48:56 +0100

    style(frontend): Added new custom features to tailwind

--- src/frontend/tailwind.config.mjs
+++ src/frontend/tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		screens: {
+			'auth-max': {'raw': '(max-width: 830px)'},
+			'auth-min': {'raw': '(min-width: 830px)'},
+			...defaultTheme.screens
+		},
+		extend: {
+			fontFamily: {
+				alfaslab: ['"Alfa Slab One"', ...defaultTheme.fontFamily.sans],
+				paytoneone: ['"Paytone One"', ...defaultTheme.fontFamily.sans],
+			},
+			flexGrow: {
+				0: '0',
+				3: '3'
+			},
+		},
 	},
 	plugins: [],
 }
