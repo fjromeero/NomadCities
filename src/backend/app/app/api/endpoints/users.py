4b6c23f5ebf9b392ad52ec35951bdc406d45fbed
fjromeero <franjaviro2001@gmail.com>
2024-01-29 18:11:57 +0100

    feat(backend): Added login api endpoint

--- src/backend/app/app/api/endpoints/users.py
+++ src/backend/app/app/api/endpoints/users.py
@@ -1,11 +1,18 @@
+from datetime import timedelta
+
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
+from app.core.settings import settings
 from app.models.user import UserOnCreate
+from app.models.token import Token
 from app.db.models.user import User
 from app.api.utils import (
     get_db,
-    hash_password
+    hash_password,
+    verify_password,
+    create_access_token,
 )
 
 router = APIRouter()
@@ -44,3 +51,24 @@ async def signup(user_data: UserOnCreate, db: Session = Depends(get_db)):
     db.refresh(user)
 
     return user.username
+
+@router.post("/login")
+async def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)) -> Token:
+    """
+    OAuth2 compatible token login with users credentials, get an access token for future requests.
+    """
+    user = db.query(User).filter(User.username == user_credentials.username).first()
+
+    if not user or not verify_password(user_credentials.password, user.hashed_password):
+        raise HTTPException(
+            status_code= 401,
+            detail= "Incorrect username or password.",
+        )
+    
+    access_token_expires = timedelta(settings.ACCESS_TOKEN_EXPIRATION_MINUTES)
+
+    return Token(
+        access_token = create_access_token(
+            user.id, expires_delta=access_token_expires
+        )
+    )
