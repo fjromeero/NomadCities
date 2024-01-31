4fcd5dafeda4d1dbcf38942112292662d238d558
fjromeero <franjaviro2001@gmail.com>
2024-01-31 17:56:21 +0100

    build(backend): Added CORS origins to api

--- src/backend/app/app/core/settings.py
+++ src/backend/app/app/core/settings.py
@@ -1,5 +1,5 @@
-from typing import Any, Dict, Optional
-from pydantic import BaseSettings, PostgresDsn, validator
+from typing import Any, Dict, Optional, Union, List
+from pydantic import BaseSettings, AnyHttpUrl, PostgresDsn, validator
 import secrets
 
 class Settings(BaseSettings):
@@ -8,6 +8,19 @@ class Settings(BaseSettings):
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
+
+    # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
+    # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
+    # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
 
     # Create a empty uri
     SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
--- src/backend/app/app/main.py
+++ src/backend/app/app/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from app.api.api import api_router
+from app.core.settings import settings
 
 app = FastAPI()
 
 app.include_router(api_router)
+
+# Set all CORS enabled origins
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
