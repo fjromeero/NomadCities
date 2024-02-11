c77e1d9df0b48704490520f1af83dd1147cb54ae
fjromeero <franjaviro2001@gmail.com>
2024-02-11 20:37:57 +0100

    refactor(frontend): Changed success banner

--- src/frontend/src/components/user/UserProfile.jsx
+++ src/frontend/src/components/user/UserProfile.jsx
@@ -10,7 +10,7 @@ export default function UserProfile({userToken}){
     return (
         <div className="w-full">
             <div className="max-w-4xl mx-auto space-y-6">
-                {successOnUpdate && <SuccessBanner/>}
+                {successOnUpdate && <SuccessBanner msg={"Perfil guardado"}/>}
                 <div className="pr-80 bg-black shadow shadow-[101010] rounded-lg auth-max:pr-0">
                     <UpdateUserProfileForm userToken={userToken} setSuccessOnUpdate={setSuccessOnUpdate}/>
                 </div>
