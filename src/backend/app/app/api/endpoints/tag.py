07ba440b366ee4a7e09048a1965984d6d54b741e
fjromeero <franjaviro2001@gmail.com>
2024-02-11 18:01:56 +0100

    refactor(backend): refactor code to avoid redundant

--- src/backend/app/app/api/endpoints/tag.py
+++ src/backend/app/app/api/endpoints/tag.py
@@ -1,19 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from typing import Any
 
 router = APIRouter()
 
-from app.api.deps import SessionDep, CurrentSuperUser
-from app.models.tag import Tag
+from app.api.deps import (
+    SessionDep,
+    get_current_superuser
+)
+from app.models.tag import Tag, Tags
 from app.crud.tag import (
     create_user_tag,
-    get_all_user_tags
+    get_all_user_tags,
 )
 
-@router.post('/usertag/create')
-async def user_tag_create(session: SessionDep, current_superuser: CurrentSuperUser, tag: Tag) -> Any:
+@router.post('/usertag', dependencies=[Depends(get_current_superuser)])
+async def user_tag_create(session: SessionDep, tag: Tag) -> Any:
     return create_user_tag(session=session, new_tag=tag)
 
 @router.get('/usertag')
-async def user_tag_get_all(session: SessionDep, current_superuser: CurrentSuperUser) -> Any:
+async def user_tag_get_all(session: SessionDep) -> Any:
     return get_all_user_tags(session=session);
---
+++ src/backend/app/app/tests/api/test_tags.py
@@ -0,0 +1,35 @@
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+from typing import Dict
+
+from app.crud.tag import search_tag, create_user_tag, user_has_tag
+from app.tests.utils.utils import random_string
+from app.models.tag import Tag
+
+name_tag = random_string(k=10)
+
+def test_create_user_tag(client: TestClient, db: Session, superuser_token_headers: Dict[str, str]) -> None:
+    r = client.post('usertag', headers=superuser_token_headers, json={
+        "name": name_tag,
+    })
+
+    assert r.status_code == 200
+    tag = r.json()
+    assert search_tag(session=db, name=tag["name"])
+
+def test_create_existing_tag(client: TestClient, superuser_token_headers: Dict[str, str]) -> None:
+    r = client.post('usertag', headers=superuser_token_headers, json={
+        "name": name_tag,
+    })
+
+    assert r.status_code == 400
+    response = r.json()
+    assert response["detail"]=="There is alredy a tag with this name"
+
+def test_create_tag_regular_user(client: TestClient, regular_user_token_headers: Dict[str, str]) -> None:
+    r = client.post('usertag', headers=regular_user_token_headers, json={
+        "name":name_tag,
+    })
+    assert r.status_code == 400
+    response = r.json()
+    assert response["detail"]=='User must be a superuser'
