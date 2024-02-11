623123d1e091cc9ca8ef5820d1a0086da1e3fd4a
fjromeero <franjaviro2001@gmail.com>
2024-02-11 18:03:01 +0100

    feat(backend): Added new user tag features

--- src/backend/app/app/crud/tag.py
+++ src/backend/app/app/crud/tag.py
@@ -1,8 +1,9 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
-
 from typing import Any
+
 from app.db.models.user_tag import UserTag
+from app.db.models.assign_user import AssignUser
 from app.models.tag import Tag
 
 def create_user_tag(*, session: Session, new_tag: Tag) -> Any:
@@ -26,3 +27,42 @@ def get_all_user_tags(*, session: Session) -> Any:
     
 def search_tag(*, session: Session, name: str) -> Any:
     return session.query(UserTag).filter(UserTag.name == name).first()
+
+def search_current_user_tags(*, session: Session, current_user_id: int):
+    return session.query(AssignUser.tag_name).filter(AssignUser.id_user == current_user_id).all()
+
+def user_has_tag(*, session: Session, user_id: int, tag_name: str):
+    return session.query(AssignUser).filter(AssignUser.id_user == user_id, AssignUser.tag_name == tag_name).first()
+
+def assign_tags_to_user(*, session: Session, user_id: int, tag_name: str):
+    tag = search_tag(session=session, name=tag_name)
+    alredy_assigned = user_has_tag(session=session, user_id=user_id, tag_name=tag_name)
+    
+    if tag and not alredy_assigned:
+        user_tag = AssignUser(
+            id_user=user_id,
+            tag_name=tag_name,
+        )
+        session.add(user_tag)
+        session.commit()
+        session.refresh(user_tag)
+        return user_tag
+    
+def remove_tags_from_user(*, session: Session, user_id: int, tag_name: str):
+    tag = search_tag(session=session, name=tag_name)
+    if tag:
+        assigned = user_has_tag(session=session, user_id=user_id, tag_name=tag_name)
+        if assigned:
+            session.delete(assigned)
+            session.commit()
+            return tag_name
+        else:
+            raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User has not this tag assigned",
+        )
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="There is no user tag with this name",
+        )
--- src/backend/app/app/models/tag.py
+++ src/backend/app/app/models/tag.py
@@ -1,4 +1,8 @@
 from pydantic import BaseModel
+from typing import List
 
 class Tag(BaseModel):
     name: str 
+
+class Tags(BaseModel):
+    tags: List[Tag]
