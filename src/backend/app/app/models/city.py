109994cbcc86af1ff97528e95b638456a81ea153
Francisco Javier Romero García <fjromeero@correo.ugr.es>
2024-03-09 13:18:50 +0100

    feat(backend): Created base and creation city model

---
+++ src/backend/app/app/models/city.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from pydantic import BaseModel
+from enum import Enum
+
+class Continents(str, Enum):
+    Africa = "Africa",
+    Antarctica = "Antarctica"
+    Asia = "Asia"
+    Europe = "Europe"
+    North_America = "North America"
+    Oceania = "Oceania"
+    South_America = "South America"
+
+class CityBase(BaseModel):
+    name: Optional[str] = None
+    country: Optional[str] = None
+    continent: Optional[Continents] = None
+    decription: Optional[str] = None
+
+# Properties to recieve via API on creation
+class CityOnCreate(CityBase):
+    name: str
+    country: str
+    continent: Continents
+    description: str
