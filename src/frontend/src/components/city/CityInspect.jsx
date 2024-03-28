6eca70fe29d09fcb83aa225d4c40dfc6bfaf184c
Francisco Javier Romero García <fjromeero@correo.ugr.es>
2024-03-28 20:04:49 +0100

    feat(frontend): Added edit city button on city inspect

--- src/frontend/src/components/city/CityInspect.jsx
+++ src/frontend/src/components/city/CityInspect.jsx
@@ -5,7 +5,7 @@ import ImageCarrousel from "./ImageCarrousel"
 import InfoSection from "./InfoSection"
 import RatingsSection from "./RatingSection"
 
-export default function CityInspect({cityData}){
+export default function CityInspect({cityData, isAdmin, id}){
     const [cityInfo, setCityInfo] = useState(cityData.info);
     const [cityRatings, setCityRatings] = useState(cityData.ratings);
     const [cityImages, setCityImages] = useState(cityData.images);
@@ -13,10 +13,17 @@ export default function CityInspect({cityData}){
     return (
         <div className="w-full">
             <ImageCarrousel images={cityImages} className={" responsive-city-image-min:hidden"}/>
-            <header className="responsive-city-image-max:pl-6">
-                <div className="max-w-7xl mx-auto responsive-city-image-max:pt-6">
+            <header className="max-w-7xl mx-auto flex justify-between auth-max:items-center">
+                <div className="responsive-city-image-max:hidden">
                     <h1 className="text-white text-3xl font-semibold font-sans">{cityInfo.name}</h1>
                 </div>
+                {
+                    isAdmin && (
+                        <a href={"/city/" + id + "/edit"} className="responsive-city-image-max:mt-6 auth-max:h-1/2 auth-max:w-20 auth-max:p-3 px-7 py-2 bg-[#7066f2] border-transparent rounded-md font-semibold text-xs uppercase tracking-widest hover:bg-slate-300 transition ease-in-out duration-150 text-center">
+                            Edit
+                        </a>
+                    )
+                }
             </header>
             <ImageBento images={cityImages} className={" responsive-city-image-max:hidden"}/>
             <InfoSection cityInfo={cityInfo}/>
