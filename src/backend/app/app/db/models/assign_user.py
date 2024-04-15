394a9579eadd209aac04f1a337f8cbabc26eb347
Francisco Javier Romero García <fjromeero@correo.ugr.es>
2024-04-16 00:02:55 +0200

    refactor(backend) Changed database models and migrations for tags

--- src/backend/app/app/db/models/assign_user.py
+++ src/backend/app/app/db/models/assign_user.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, ForeignKey, String, Integer
+from sqlalchemy import Column, ForeignKey, Integer
 from app.db.engine import Base
 
 class AssignUser(Base):
     __tablename__ = 'assign_user'
 
     id_user = Column(Integer, ForeignKey('user.id'), primary_key=True, index=True)
-    tag_name = Column(String, ForeignKey('user_tag.name'), primary_key=True, index=True)
+    id_user_tag = Column(Integer, ForeignKey('user_tag.id'), primary_key=True, index=True)
--- src/backend/app/app/db/models/city_tag.py
+++ src/backend/app/app/db/models/city_tag.py
@@ -1,7 +1,11 @@
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from app.db.models.tag import Tag
 
 class CityTag(Tag):
     __tablename__ = 'city_tag'
 
-    name = Column(String, ForeignKey('tag.name'), primary_key=True, index=True)
+    id = Column(Integer, ForeignKey('tag.id'), primary_key=True, index=True)
+
+    __mapper_args__ = {
+        "polymorphic_identity": "name",
+    }
