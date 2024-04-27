226a5a2939e82b81dc3e16e9bbb608665a39a195
Francisco Javier Romero García <fjromeero@correo.ugr.es>
2024-04-27 14:19:42 +0200

    style(frontend): Update modals styles

--- src/frontend/src/components/city/ImageCarrousel.jsx
+++ src/frontend/src/components/city/ImageCarrousel.jsx
@@ -3,12 +3,18 @@ import { useRef } from "react";
 
 export default function ImageCarrousel({ images, className }) {
     const modalRef = useRef();
+
+    const showModal = () => {
+        modalRef.current.showModal();
+        document.body.classList.add('overflow-y-hidden')
+    }
+
     return (
         <div>
             <div className={" slider relative overflow-x-scroll flex aspect-auto max-h-[300px]" + className} style={{ scrollSnapType: "x mandatory", "scrollbarWidth": "none" }}>
                 {
                     images.map((image, index) => (
-                        <img key={index} className="w-full left-0 sticky object-cover" style={{ scrollSnapAlign: "center" }} src={image.path} alt={`City images`} onClick={() => modalRef.current.showModal()} />
+                        <img key={index} className="w-full left-0 sticky object-cover" style={{ scrollSnapAlign: "center" }} src={image.path} alt={`City images`} onClick={() => showModal()} />
                     ))
                 }
             </div>
--- src/frontend/src/components/city/ImageGallery.jsx
+++ src/frontend/src/components/city/ImageGallery.jsx
@@ -2,11 +2,17 @@ import Modal from "../Modal";
 import { forwardRef } from "react";
 
 const ImageGallery = forwardRef(function ImageGallery({images}, ref) {
+
+    const closeModal = () => {
+        ref.current.close();
+        document.body.classList.remove("overflow-y-hidden");
+    }
+
     return (
         <Modal ref={ref} className="max-w-full w-full max-h-full h-full bg-[#121212]">
             <header className="sticky top-0 left-0 right-0 bg-[#121212]">
                 <div className="py-5 pl-5">
-                    <button onClick={() => ref.current.close()}>
+                    <button onClick={() => closeModal()}>
                         <svg
                             viewBox="0 0 512 512"
                             fill="none"
