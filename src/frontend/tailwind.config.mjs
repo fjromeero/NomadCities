c988c2afc705213bc63b222f0dad661f5491cfdc
Francisco Javier Romero García <fjromeero@correo.ugr.es>
2024-06-20 18:19:47 +0200

    styles(frontend): Add custom tailwind rules

--- src/frontend/tailwind.config.mjs
+++ src/frontend/tailwind.config.mjs
@@ -4,19 +4,19 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	
+
 	theme: {
 		screens: {
-			'auth-max': {'raw': '(max-width: 830px)'},
-			'auth-min': {'raw': '(min-width: 830px)'},
-			'responsive-city-image-max': {'raw': '(max-width: 1200px)'},
-			'responsive-city-image-min': {'raw': '(min-width: 1200px)'},
-			'mobile': {'raw': '(max-width: 640px)'},
-			'main-1': {'raw': '(max-width: 2200px)'},
-			'main-2': {'raw': '(max-width: 1775px)'},
-			'main-3': {'raw': '(max-width: 1250px)'},
-			'main-4': {'raw': '(max-width: 950px)'},
-			'main-5': {'raw': '(max-width: 600px)'},
+			'auth-max': { 'raw': '(max-width: 830px)' },
+			'auth-min': { 'raw': '(min-width: 830px)' },
+			'responsive-city-image-max': { 'raw': '(max-width: 1200px)' },
+			'responsive-city-image-min': { 'raw': '(min-width: 1200px)' },
+			'mobile': { 'raw': '(max-width: 640px)' },
+			'main-1': { 'raw': '(max-width: 2200px)' },
+			'main-2': { 'raw': '(max-width: 1775px)' },
+			'main-3': { 'raw': '(max-width: 1250px)' },
+			'main-4': { 'raw': '(max-width: 950px)' },
+			'main-5': { 'raw': '(max-width: 600px)' },
 			...defaultTheme.screens
 		},
 		extend: {
@@ -41,6 +41,7 @@ export default {
 				'main-4': 'repeat(2, minmax(0, 1fr))',
 				'main-5': 'repeat(1, minmax(0, 1fr))',
 				'comments-modal': '333px auto',
+				'tag': 'repeat(auto-fit, minmax(130px, 1fr))',
 			},
 			gridAutoRows: {
 				'bento': 'minmax(100px, 270px)',
@@ -49,7 +50,7 @@ export default {
 			},
 			boxShadow: {
 				'tag-selected': '0 0 0 3px white',
-			},
+			}
 		},
 	},
 	plugins: [],
